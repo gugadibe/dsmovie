f9f8ee7c870b642624dc6f43e8f8d3cdc116d57b
Gustavo Dibe <gugadibe@gmail.com>
2022-05-14 12:33:05 -0300

    Save, useNavigate

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,22 +1,24 @@
-import {ReactComponent as GithubIcon} from "assets/img/github.svg";
-import "./styles.css"
-function Navbar(){
-
-    return (
-        <header>
-        <nav className="container">
-          <div className="dsmovie-nav-content">
+import { ReactComponent as GithubIcon } from "assets/img/github.svg";
+import { Link } from "react-router-dom";
+import "./styles.css";
+function Navbar() {
+  return (
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <Link to="/">
             <h1>DSMovie</h1>
-            <a href="https://github.com/gugadibe/dsmovie">
-              <div className="dsmovie-contact-container">
-                <GithubIcon />
-                <p className="dsmovie-contact-link">/devsuperior</p>
-              </div>
-            </a>
-          </div>
-        </nav>
-      </header>
-    )
+          </Link>
+          <a href="https://github.com/gugadibe/dsmovie">
+            <div className="dsmovie-contact-container">
+              <GithubIcon />
+              <p className="dsmovie-contact-link">/devsuperior</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;
